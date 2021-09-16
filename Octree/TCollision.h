e2bd58ddc4aaeaef543b7967edba16b53266ad33
prokvip <prokvip@msn.com>
2021-09-16 11:28:25 +0900

    update

--- Octree/TCollision.h
+++ Octree/TCollision.h
@@ -3,6 +3,15 @@
 class TCollision
 {
 public:
+	//// 2D ->  화면좌표계
+	//static bool  LineToPoint(TRect desk, TRect src) { return true; };
+	//static bool  LineToSphere2D(TRect desk, TRect src) { return true; };
+	//static bool  RectToRect2D(TRect desk, TRect src) { return true; };
+	//static bool  RectToPoint2D(TRect desk, TVector3 pos) { return true; };
+	//static TRect UnionRegion(const TRect& r1, const TRect& r2);
+	//static bool IntersectRegion(const TRect& r1, const TRect& r2, TRect* rtIntersect);
+
+	// 3D - > 직각좌표계
 	static bool  LineToPoint(TRect desk, TRect src) { return true; };
 	static bool  LineToSphere(TRect desk, TRect src) { return true; };
 	static bool  RectToRect(TRect desk, TRect src) { return true; };
