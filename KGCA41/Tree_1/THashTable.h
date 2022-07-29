512c317b8ab8ffef7ef55517307294d819933b72
prokvip <prokvip@msn.com>
2022-07-29 17:19:26 +0900

    update

---
+++ KGCA41/Tree_1/THashTable.h
@@ -0,0 +1,164 @@
+#pragma once
+#include "TLinkedList.h"
+template < class K, class T>
+class TBstNode
+{
+public:
+	K			m_key;
+	T			m_Data;
+	int			m_iDepth;
+	TBstNode*	m_pChild[2];
+	TBstNode()
+	{
+		m_pChild[0] = nullptr;
+		m_pChild[1] = nullptr;		
+		m_iDepth = 0;
+	}
+	virtual ~TBstNode(){}
+};
+
+template < class K, class T>
+class TBucket
+{
+public:
+	K m_Key;
+	T m_pData;
+	TBucket(K k, T t)
+	{
+		m_Key = k;
+		m_pData = t;
+	}
+	~TBucket() {
+	}
+};
+template <class T>
+class TBucket<char*,T>
+{
+public:
+	char* m_Key;
+	T m_pData;
+	TBucket(char* k, T t)
+	{
+		m_Key = new char[strlen(k)+1];
+		memcpy(m_Key, k, sizeof(char) * strlen(k) + 1);
+		m_pData = t;
+	}
+	~TBucket() {
+		delete m_Key;
+		m_Key = nullptr;
+	}
+};
+template < class K, class T>
+class TTable
+{
+public:
+	//typedef TBucket<K, T> TBUCKET;
+	using TBUCKET = TBucket<K, T>;
+	TLinkedList<TBUCKET*>	m_Table;// [10] ;
+	TBstNode<K, T>*			m_pBstRoot;
+public:
+	int			hash(K key)// int
+	{
+		unsigned long hash = 0x45AB904C;
+		unsigned long val = static_cast<unsigned long>(key);
+		hash = (hash + val) << 5;
+		return (hash ^ (hash >> 16)) % 10;		
+	}	
+	void		insert(K k, T t);
+	void		clear();
+	T			bst(K key);
+	int			size()
+	{
+		return m_Table.size();
+	}
+	void		CreateBinarySearchTree(TBstNode<K,T>* pBstNode, TBUCKET* pData);
+	TBstNode<K, T>* RecursiveBST(TBstNode<K, T>* pNode, K key);
+};
+template < class K, class T>
+T TTable<K, T>::bst(K key)
+{
+	TBstNode<K, T>* pFindNode = RecursiveBST(m_pBstRoot, key);
+	return pFindNode->m_Data;
+}
+template < class K, class T>
+TBstNode<K, T>* TTable<K, T>::RecursiveBST(TBstNode<K, T>* pNode, K key)
+{
+	if (pNode == nullptr) return nullptr;
+	if (pNode->m_key == key) return pNode;
+	if (pNode->m_key >= key)
+	{
+		RecursiveBST(pNode->m_pChild[0], key);
+	}
+	else
+	{
+		RecursiveBST(pNode->m_pChild[1], key);
+	}
+}
+template < class K, class T>
+void	TTable<K, T>::clear()
+{
+	//using DATA2 = TBucket<char*, TItem*>*;
+	//using TNODE2 = TNode<DATA2>*;
+	if (!m_Table.empty())
+	{
+		for (TNode<TBUCKET*>* pNode = m_Table.begin();
+			 pNode != m_Table.end();
+			pNode = m_Table.next())
+		{
+			TBUCKET* pData = pNode->m_pData;
+			if (pData) delete pData;
+		}
+		m_Table.clear();
+	}
+}
+template < class K, class T>
+void	TTable<K,T>::insert(K k, T t)
+{
+	TBucket<K, T>* pNewBucket = new TBucket<K, T>(k, t);
+	int hash = pNewBucket->hash(k);
+	//m_Table[hash].push_back(pNewBucket);
+	m_Table.push_back(pNewBucket);
+	if (m_pBstRoot == nullptr)
+	{
+		m_pBstRoot = new TBstNode<K, T>;
+		m_pBstRoot->m_key = k;
+		m_pBstRoot->m_Data = pNewBucket->m_pData;
+		return;
+	}
+	CreateBinarySearchTree(m_pBstRoot, pNewBucket);
+}
+template < class K, class T>
+void TTable<K, T>::CreateBinarySearchTree(TBstNode<K, T>* pBstNode,
+	TBUCKET* pData)
+{
+	// 루트 >= iValue ==> 왼쪽
+	if (pBstNode->m_key >= pData->m_Key)
+	{
+		if (pBstNode->m_pChild[0] == nullptr)
+		{
+			pBstNode->m_pChild[0] = new TBstNode<K, T>();
+			pBstNode->m_pChild[0]->m_key = pData->m_Key;
+			pBstNode->m_pChild[0]->m_Data = pData->m_pData;
+			pBstNode->m_pChild[0]->m_iDepth = pBstNode->m_iDepth + 1;
+		}
+		else
+		{
+			CreateBinarySearchTree(pBstNode->m_pChild[0], pData);
+		}
+	}
+	// 루트 <  iValue ==> 오른쪽
+	else
+	{
+		if (pBstNode->m_pChild[1] == nullptr)
+		{
+			pBstNode->m_pChild[1] = new TBstNode<K, T>();
+			pBstNode->m_pChild[1]->m_key = pData->m_Key;
+			pBstNode->m_pChild[1]->m_Data = pData->m_pData;
+			pBstNode->m_pChild[1]->m_iDepth = pBstNode->m_iDepth + 1;
+		}
+		else
+		{
+			CreateBinarySearchTree(pBstNode->m_pChild[1], pData);
+		}
+	}
+}
---
+++ KGCA41/Tree_1/Tree_1.cpp
@@ -0,0 +1,164 @@
+﻿// Tree_1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include "THashTable.h"
+//enum TTreeDir { T_LEFT = 0, T_RIGHT, };
+//class TNode
+//{
+//public:
+//	int		m_iIndex;
+//	int		m_iDepth;
+//	TNode* m_pChild[2];
+//	TNode* m_pParent;
+//	TNode() : m_iIndex(-1), m_iDepth(0)
+//	{
+//		for (int iNode = 0; iNode < 2; iNode++)
+//		{
+//			m_pChild[iNode] = nullptr;
+//		}
+//		m_pParent = nullptr;
+//	}
+//	virtual ~TNode()
+//	{
+//		std::cout << this->m_iIndex << " ";
+//	}
+//};
+int g_iData[] = { 5,23,67,3,7,3,6,3,1,8 };
+//int g_iCounter = 0;
+//TNode* g_pRoot = nullptr;
+//
+//void CreateBinarySearchTree(TNode* pNode, int iValue)
+//{
+//	// 루트 >= iValue ==> 왼쪽
+//	if( pNode->m_iIndex >= iValue)
+//	{
+//		if (pNode->m_pChild[0] == nullptr)
+//		{
+//			pNode->m_pChild[0] = new TNode();
+//			pNode->m_pChild[0]->m_iIndex = iValue;
+//			pNode->m_pChild[0]->m_iDepth = pNode->m_iDepth + 1;
+//		}
+//		else
+//		{
+//			CreateBinarySearchTree(pNode->m_pChild[0], iValue);
+//		}
+//	}
+//	// 루트 <  iValue ==> 오른쪽
+//	else
+//	{ 
+//		if (pNode->m_pChild[1] == nullptr)
+//		{
+//			pNode->m_pChild[1] = new TNode();
+//			pNode->m_pChild[1]->m_iIndex = iValue;
+//			pNode->m_pChild[1]->m_iDepth = pNode->m_iDepth + 1;
+//		}
+//		else
+//		{
+//			CreateBinarySearchTree(pNode->m_pChild[1], iValue);
+//		}
+//	}
+//}
+//TNode* BinarySearchTree(TNode* pNode, int iValue)
+//{
+//	if (pNode == nullptr) return nullptr;
+//	if (pNode->m_iIndex == iValue) return pNode;
+//	if (pNode->m_iIndex >= iValue)
+//	{
+//		BinarySearchTree(pNode->m_pChild[0], iValue);		
+//	}
+//	else
+//	{	
+//		BinarySearchTree(pNode->m_pChild[1], iValue);		
+//	}
+//}
+
+struct TItem
+{
+	int iValue;
+	char name[4];
+	TItem(int i) : iValue(i) {
+		name[0] = 65 + rand() % 26;
+		name[1] = 65 + rand() % 26;
+		name[2] = 97 + rand() % 26;
+		name[3] = 0;
+	}
+};
+
+template<class T>
+void Print(TLinkedList<T>& list)
+{
+	for (TNode<T>* node = list.m_pHead->m_pNext;
+		node != list.m_pTail;
+		node = node->m_pNext)
+	{
+		int iData = node->m_pData;
+		std::cout << iData << " ";
+	}
+	std::cout << std::endl;
+}
+
+void main()
+{
+	srand(time(NULL));
+	TTable<int, TItem*> list;
+	for (int iCnt = 0; iCnt < 10; iCnt++)
+	{
+		TItem* pItem = new TItem(rand());
+		list.insert(pItem->iValue, pItem);
+	}
+	TItem* pFindItem = list.bst(8);
+
+	using DATA = TBucket<int, TItem*>*;
+	using TNODE = TNode<DATA>*;
+	for (TNODE pBucket = list.m_Table.begin();
+		pBucket != list.m_Table.end();
+		pBucket = list.m_Table.next())
+	{
+		DATA pData = pBucket->m_pData;
+		std::cout << pData->m_Key << ":" << pData->m_pData->iValue << std::endl;
+	}
+	std::cout << std::endl;
+	list.clear();
+	/*TTable<char*, TItem*> list2;
+	for (int iCnt = 0; iCnt < 10; iCnt++)
+	{
+		TItem* pItem = new TItem(10 + iCnt);
+		list2.insert(pItem->name, pItem);
+	}
+	using DATA2 = TBucket<char*, TItem*>*;
+	using TNODE2 = TNode<DATA2>*;
+	for (TNODE2 pBucket = list2.m_Table.begin();
+		pBucket != list2.m_Table.end();
+		pBucket = list2.m_Table.next())
+	{
+		DATA2 pData = pBucket->m_pData;
+		std::cout << pData->m_Key << ":" << pData->m_pData->iValue << std::endl;
+	}
+	std::cout << std::endl;	
+	list2.clear();*/
+}
+
+//int main()
+//{
+//	g_pRoot = new TNode();
+//	g_pRoot->m_iIndex = g_iData[g_iCounter++];	
+//	for (int i = 1; i < _countof(g_iData); i++)
+//	{
+//		CreateBinarySearchTree(g_pRoot, g_iData[i]);
+//	}
+//
+//	for (int i = 0; i < _countof(g_iData); i++)
+//	{
+//		TNode* pFind = BinarySearchTree(g_pRoot, g_iData[i]);
+//		if (pFind != nullptr)
+//		{
+//			std::cout << " "<< pFind->m_iIndex;
+//		}
+//		else
+//		{
+//			int k = 0;
+//		}
+//	}
+//	std::cout << "Hello World!\n";
+//}
