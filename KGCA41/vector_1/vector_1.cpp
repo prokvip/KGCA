01c0e3b76312ba7184eaf7332e45d441010cc580
prokvip <prokvip@msn.com>
2022-08-01 15:09:29 +0900

    update

--- KGCA41/vector_1/vector_1.cpp
+++ KGCA41/vector_1/vector_1.cpp
@@ -27,69 +27,155 @@ struct IsCmp
         return (item->iValue == m_iValue);
     }
 };
-bool  compare(TItem* a, TItem* b)
+struct IsCmp2
+{
+    int m_iValue;
+    IsCmp2(int value) : m_iValue(value) {}
+    bool operator()(const TItem& item)
+    {
+        return (item.iValue == m_iValue);
+    }
+};
+bool  compareInt(const int& a, const int& b)
+{
+    return a < b;
+}
+bool  compare(const TItem& a, const TItem& b)
+{
+    return a.iValue < b.iValue;
+}
+bool  comparePtr( TItem* a, TItem* b)
 {
     return a->iValue < b->iValue;
 }
 int g_iFindValue;
-bool  find_TItem(TItem* a)
+bool  find_TItem(const TItem& a)
 {
-    return a->iValue == g_iFindValue;
+    return a.iValue == g_iFindValue;
 }
 int main()
 {
-    /*std::vector<TItem>  list2;
-    list2.push_back(17);
-    TItem finditem3(17);
-    std::vector<TItem>::iterator iter3 =
-        std::find(list2.begin(), list2.end(), finditem3);*/
-
-    std::vector<TItem*>  list;  
+    /// <summary>
+    /// ///////////////////////////////
+    /// </summary>
+    /// <returns></returns>
+    std::vector<int>  listInt;
     for (int i = 0; i < 20; i++)
     {
-        list.push_back(new TItem(i));
+        listInt.push_back(i);
     }
     for (int i = 0; i < 5; i++)
     {
-        list.insert(list.begin(), new TItem(10+i));
+        listInt.insert(listInt.begin(),10+i);
     }
     for (int i = 0; i < 5; i++)
     {
-        list.insert(list.end(), new TItem(20 + i));
+        listInt.insert(listInt.end(), 20 + i);
     }
-    std::sort(list.begin(), list.end(), compare);
-     // find_if -> operator ()
-    std::vector<TItem*>::iterator iter = std::find_if(list.begin(), list.end(), IsCmp(17));
-    list.erase(iter);
+    int iValue1 = listInt.front();
+    int iValue2 = listInt.back();
+    listInt.pop_back();
 
-    //int iValue1 = list.front();
-    //int iValue2 = list.back();
-    //list.pop_back();
+    for (std::vector<int>::iterator iter = listInt.begin();
+        iter != listInt.end();
+        iter++)
+    {
+        //int* iValue = iter._Ptr; // *iter        
+        std::cout << " " << *iter;
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < listInt.size(); i++)
+    {
+        std::cout << " " << listInt[i];
+    }
+    std::cout << std::endl;
+    for (int value : listInt) // 복사
+    {
+        value = 100;
+        std::cout << " " << value;
+    }
+    std::cout << std::endl;
+    /*for (int& value : listInt)
+    {
+        value = 100;
+        std::cout << " " << value;
+    }*/
+    std::sort(listInt.begin(), listInt.end(), compareInt);
+    std::vector<int>::iterator iterint = std::find(listInt.begin(), listInt.end(), 17);    
+    if (iterint != listInt.end())
+    {
+        listInt.erase(iterint);
+    }
+    listInt.clear();
+    /// <summary>
+    /// //
+    /// </summary>
+    /// <returns></returns>
+    std::vector<TItem>  listitem;
+    for (int i = 0; i < 20; i++)
+    {
+        TItem item(i);
+        listitem.push_back(item);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        TItem item(10 + i);
+        listitem.insert(listitem.begin(), item);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        TItem item(20 + i);
+        listitem.insert(listitem.end(), item);
+    }
+    std::sort(listitem.begin(), listitem.end(), compare);
+    //// find_if -> operator ()
+    std::vector<TItem>::iterator iter2 = std::find_if(listitem.begin(), listitem.end(), IsCmp2(17));
+    if (iter2 != listitem.end())
+    {
+        listitem.erase(iter2);
+    }
+   
+    TItem finditem3(20);
+    std::vector<TItem>::iterator iteritem =
+        std::find(listitem.begin(), listitem.end(), finditem3);
+    if (iteritem != listitem.end())
+    {
+        listitem.erase(iteritem);
+    }
+    listitem.clear();
 
-    //for (std::vector<int>::iterator iter = list.begin();
-    //    iter != list.end();
-    //    iter++)
-    //{
-    //    //int* iValue = iter._Ptr; // *iter        
-    //    std::cout << " " << *iter;
-    //}
-    //std::cout << std::endl;
-    //for (int i = 0; i < list.size(); i++)
-    //{
-    //    std::cout << " " << list[i];
-    //}
-    //std::cout << std::endl;
-    //for (int value : list) // 복사
-    //{
-    //    value = 100;
-    //    std::cout << " " << value;
-    //}
-    //std::cout << std::endl;
-    //for (int& value : list)
-    //{
-    //    value = 100;
-    //    std::cout << " " << value;
-    //}
-    list.clear();
+    
+
+    /// <summary>
+    /// ////
+    /// </summary>
+    /// <returns></returns>
+    std::vector<TItem*>  listptr;
+    for (int i = 0; i < 20; i++)
+    {
+        listptr.push_back(new TItem(i));
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        listptr.insert(listptr.begin(), new TItem(10 + i));
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        listptr.insert(listptr.end(), new TItem(20 + i));
+    }
+    std::sort(listptr.begin(), listptr.end(), comparePtr);
+    // find_if -> operator ()
+    std::vector<TItem*>::iterator iterptr = std::find_if(listptr.begin(), listptr.end(), IsCmp(17));
+    if (iterptr != listptr.end())
+    {
+        delete *iterptr;
+        listptr.erase(iterptr);
+    }
+
+    for (int i = 0; i < listptr.size(); i++)
+    {
+        delete listptr[i];
+    }
+    listptr.clear();
     std::cout << "Hello World!\n";
 }
