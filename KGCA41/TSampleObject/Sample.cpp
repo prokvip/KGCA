53fcd4618931642974c62c18b79b9ea8e5afeb36
prokvip <prokvip@msn.com>
2022-09-13 13:43:19 +0900

    update

--- KGCA41/TSampleObject/Sample.cpp
+++ KGCA41/TSampleObject/Sample.cpp
@@ -17,7 +17,7 @@ bool Sample::Init()
    // -1 ~ +1
 	m_pUser = new TUser2D;
 	m_pUser->Create(m_pd3dDevice, m_pImmediateContext,		
-		L"DefaultShapeMask.txt",
+		L"../../data/shader/DefaultShapeMask.txt",
 		L"../../data/bitmap1.bmp" );
 	m_pUser->SetMask(pMaskTex);
 	m_pUser->m_fSpeed = 300.0f;
@@ -30,7 +30,7 @@ bool Sample::Init()
 	{
 		TNpc2D* npc = new TNpc2D;
 		npc->Create(m_pd3dDevice, m_pImmediateContext,			
-			L"DefaultShapeMask.txt",
+			L"../../data/shader/DefaultShapeMask.txt",
 			L"../../data/bitmap1.bmp");
 		if (iNpc % 2 == 0)
 		{
