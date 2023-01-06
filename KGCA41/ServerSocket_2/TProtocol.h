8578b415bef2f73f0567573a4170013e6c1ae53b
prokvip <prokvip@msn.com>
2023-01-06 12:11:10 +0900

    er

--- KGCA41/ServerSocket_2/TProtocol.h
+++ KGCA41/ServerSocket_2/TProtocol.h
@@ -12,7 +12,24 @@
 		PACKET_HEADER ph;
 		char   msg[2048];
 	}UPACKET;
+
+	///   케릭터 생성 패킷
+	typedef struct {
+		byte    szName[13];
+		int		iIndex;
+		int		iData[50];
+	}CHARACTER_INFO;
+	///   아이템 생성 패킷
+	typedef struct {
+		byte    szName[13];
+		int		iIndex;
+		int		iData[10];
+	}ITEM_INFO;
 #pragma pack(pop)
 
 #define PACKET_CHAR_MSG   1000
+#define PACKET_GAME_START    2000
+#define PACKET_GAME_END    3000
+#define PACKET_CREATE_CHARACTER    4000
+#define PACKET_MOVE_CHARACTER    5000
 
