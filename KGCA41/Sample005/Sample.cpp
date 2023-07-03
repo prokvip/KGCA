2e874040cef7ed08b71d38754d23505137ead265
prokvip <prokvip@msn.com>
2023-07-03 14:05:32 +0900

    uml

--- KGCA41/Sample005/Sample.cpp
+++ KGCA41/Sample005/Sample.cpp
@@ -1,49 +1,4 @@
 ﻿#include "TStudentManager.h"
-// 함수템플릿, 
-// 클래스템플릿
-//void Fun1(int a = 9)
-//{
-//    std::cout << a;
-//}
-//void Fun1(int* a=nullptr)
-//{
-//    std::cout << *a;
-//}
-//void swap(int x, int y)
-//{
-//    int temp;
-//    temp = x;
-//    x = y;
-//    y = temp;
-//}
-//void swap(int& x, int& y)
-//{
-//    int temp;
-//    temp = x;
-//    x = y;
-//    y = temp;
-//}
-//void swap(int* a, int* b)
-//{
-//    int temp;
-//    temp = *a;
-//    *a = *b;
-//    *b = temp;
-//}
-//void swap(float& x, float& y)
-//{
-//    int temp;
-//    temp = x;
-//    x = y;
-//    y = temp;
-//}
-//void swap(float* a, float* b)
-//{
-//    int temp;
-//    temp = *a;
-//    *a = *b;
-//    *b = temp;
-//}
 template<class T>
 void Swap(T& a, T& b)
 {
@@ -118,53 +73,9 @@ void Swap(const char* a, const char* b)
     //a = b;
     //b = temp;
 }
-TStudent Func()
-{
-    int a = 88;
-    TStudent s(a,"fff");
-    return s;
-}
-//void Function(TStudent src)
-//{
-//    std::cout << src.m_iID;
-//}
-void Function(TStudent* src)
-{
-    std::cout << src->m_iID;
-}
-void Function(TStudent& src)
-{
-    std::cout << src.m_iID;
-}
-void Function(TStudent&& src)
-{
-    std::cout << src.m_iID;
-}
+
 int main()
-{       
-    TStudent t1;
-    TStudent t2 = t1;
-    TStudent t5 = TStudent(7,"aaa");    // 일반 생성자 호출(t5)
-    TStudent&& t6 = TStudent(8, "bbb"); // 일반 생성자 호출(t6)
-    TStudent t7 = Func();
-    TStudent t8 = std::move(TStudent(9, "ccc"));
-    TStudent&& t9 = std::move(TStudent(11, "ddd"));
-
-
-    TStudent t3,t4;
-    t3 = t4;
-    t3 = TStudent(1);
-
-    TStudent* p = new TStudent;
-    Function(*p);
-    Function(std::move(*p));
-    TStudent s;
-    Function(s);
-    int a = 4;
-    int& b = a;   
-    Function(TStudent());
-    int&& i = 3;
-
+{   
     TStudentManager mgr;
     mgr.Init();
     mgr.Run();
--- KGCA41/Sample006/Sample.cpp
+++ KGCA41/Sample006/Sample.cpp
@@ -20,9 +20,79 @@ struct TWidget
     //friend std::ostream& operator << (  std::ostream& os, 
     //                                    const TWidget& s);
 };
+template<typename t1, typename t2>
+auto add(t1 a, t2 b)-> decltype(a+b)
+{
+    return (int)(a + b);
+}
+// 새로운 함수 정의 문법
+auto Get() -> int
+{
+    float i = 9.3f;
+    return i+4;
+}
+int GetInt()
+{
+    int i = 9;
+    return i;
+}
+auto PRINT() -> void
+{
+    std::cout << "kgca";
+}
 
 int main()
-{   
+{  
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    auto fun = [](int i, int j) 
+    { 
+        return i + j; 
+    };
+    int iRet = fun(a,b);
+    //auto GetInt() -> int
+    //무명함수
+    PRINT(); // 캡쳐절
+    auto Fun1 = []()->void{ std::cout << "kgca"; };
+    auto Fun2 = []() { std::cout << "kgca"; };
+    auto Fun3 = [] { std::cout << "kgca"; };
+
+    Fun3();
+    Fun3();
+
+    Fun3();
+    Fun3();
+    Fun3();
+    Fun3();
+    Fun3();
+    Fun1();
+
+    int jValue = Get();
+    std::cout << jValue;
+
+    int i = 9;
+    // 새로운 변수가 생성된다.
+    auto Function1 = [i]() { return i; };
+    int iValue1 = Function1();
+    std::cout << iValue1;
+    // 참조로 처리된다.
+    auto Function2 = [&i]() -> int { return i += 1; };
+    int iValue2 = Function2();
+    std::cout << iValue2;
+    
+
+    //// 컴파일러 결정(추론)
+    //auto ret = Get();
+    ////int (*Func)() = Get;
+    //auto Func = Get;
+    //Func();
+    //// // 컴파일러 결정(추론)
+    //decltype(3) myInt = 5;
+    //decltype("str") myString = "str";
+    //decltype(3.14) myFloat = 3.14;
+
+
     TLinkedlist<TWidget> list;
     list.Init();
     for (int i = 0; i < 3; i++)
