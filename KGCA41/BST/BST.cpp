eb0e4b61e5a8c005afb9f63988d74d99a9f8a4fd
prokvip <prokvip@msn.com>
2023-07-10 12:37:44 +0900

    bst

--- KGCA41/BST/BST.cpp
+++ KGCA41/BST/BST.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <queue>
 struct TNode
 {
     int    m_iValue = -1;
@@ -13,7 +14,7 @@ struct TNode
 };
 TNode* g_pRootNode = nullptr;
 
-int   g_iData[] = { 50,76,21,4,32,64,15,14,100,83,2,3,70,87,80 };
+int   g_iData[] = { 50,76,21,4,32,64,15,52,14,100,83,2,3,70,87,80 };
 int   g_iIndex=0;
 void  AddNode(TNode* pParentNode, TNode* pNewNode)
 {
@@ -71,6 +72,50 @@ TNode*  Find(TNode* pParent, int iFind)
         }
     }   
 }
+
+
+void PreOrder(TNode* pNode)
+{
+    if (pNode == nullptr) return;
+    std::cout << pNode->m_iValue << " ";
+    PreOrder(pNode->m_pLeftChild);
+    PreOrder(pNode->m_pRightChild);
+}
+void InOrder(TNode* pNode)
+{
+    if (pNode == nullptr) return;
+    InOrder(pNode->m_pLeftChild);
+    std::cout << pNode->m_iValue << " ";
+    InOrder(pNode->m_pRightChild);
+}
+void PostOrder(TNode* pNode)
+{
+    if (pNode == nullptr) return;
+    PostOrder(pNode->m_pLeftChild);
+    PostOrder(pNode->m_pRightChild);
+    std::cout << pNode->m_iValue << " ";
+}
+
+std::queue<TNode*> g_Queue;
+void LevelOrder(TNode* pNode)
+{
+    if (pNode == nullptr) return;
+    std::cout << pNode->m_iValue << " ";
+    if (pNode->m_pLeftChild != nullptr)
+    {        
+        g_Queue.push(pNode->m_pLeftChild);
+    }
+    if (pNode->m_pRightChild != nullptr)
+    {
+        g_Queue.push(pNode->m_pRightChild);
+    }   
+    if (!g_Queue.empty())
+    {
+        TNode* pNode = g_Queue.front();
+        g_Queue.pop();
+        LevelOrder(pNode);
+    }
+}
 int main()
 {
     g_pRootNode = new TNode(g_iData[g_iIndex++]);
@@ -82,5 +127,12 @@ int main()
 
     TNode * pFind = Find(g_pRootNode, g_iData[10]);
     pFind = Find(g_pRootNode, 99);
+
+    // P L R
+    PreOrder(g_pRootNode); std::cout << std::endl;
+    // L P R
+    InOrder(g_pRootNode); std::cout << std::endl;
+    // L R P
+    PostOrder(g_pRootNode);
     std::cout << "Hello World!\n";
 }
