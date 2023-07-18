1ec6742ba1870352c2e0ba27656e935d00e4cd7b
prokvip <prokvip@msn.com>
2023-07-18 14:30:56 +0900

    123

---
+++ KGCA41/Quadtree3/Sample.cpp
@@ -0,0 +1,110 @@
+﻿#include "TQuadtree.h"
+#include "TTimer.h"
+std::vector<TObject*> StaticObjectList;
+std::vector<TObject*> DynamicObjectList;
+
+void SetStaticObject(TQuadtree& quadtree)
+{    
+    for (int i = 0; i < 10; i++)
+    {
+        TObject* obj = new TStaticObject();
+        obj->m_csName = L"StaticObj";
+        obj->m_csName += std::to_wstring(i);
+        obj->m_Position = { (float)(rand() % 800), (float)(rand() % 600) };
+        obj->SetRect(obj->m_Position, 
+                    (float)((rand() % 10) + 1.0f), 
+                    (float)((rand() % 10) + 1.0f));
+        
+        quadtree.StaticAddObject(obj);
+        StaticObjectList.push_back(obj);
+    }
+}
+void SetDynamicObject(TQuadtree& quadtree)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        TObject* obj = new TDynamicObject();
+        
+        obj->m_csName = L"DynamicObj";
+        obj->m_csName += std::to_wstring(i);
+        obj->m_Position = { (float)(rand() % 800), (float)(rand() % 600) };
+        obj->SetRect(obj->m_Position,
+            (float)((rand() % 10) + 1.0f),
+            (float)((rand() % 10) + 1.0f));
+
+        TPoint d = {  (float)randstep(0, 800), 
+                     (float)randstep(0, 600) };
+        obj->SetTarget(d);
+        quadtree.DynamicAddObject(obj);
+        DynamicObjectList.push_back(obj);
+    }
+}
+int main()
+{    
+    TRect rt1, rt2;    
+    rt1.Set(TPoint(0,0), 100, 100);
+    rt2.Set(TPoint(50, 50), 100, 100);
+    if (TCollision::RectToRect(rt1, rt2))
+    {
+        std::cout << " collision" << std::endl;    
+    }
+    TRect rt = rt1 - rt2;
+    if (rt.m_bEnable)
+    {
+        std::cout << "Collision" << std::endl;
+    }
+  
+    //std::set<int> setList;
+    //setList.insert(1);
+    //setList.insert(5);
+    //setList.insert(1);
+    //setList.insert(4);
+    //setList.insert(7);
+    //setList.insert(5);
+
+    TQuadtree quadtree;
+    quadtree.BuildQuadtree();   
+    SetStaticObject(quadtree);
+    SetDynamicObject(quadtree);
+
+    TTimer  timer;
+    timer.Init();
+    // 프레임 고정 100프레임    float fStep = 1.0f / 50.0f;
+    while (timer.m_fGameTimer < 1000.0f)
+    {
+        timer.Frame();       
+        quadtree.PreFrame();
+
+        for (int i = 0; i < DynamicObjectList.size(); i++)
+        {
+            TObject* obj = DynamicObjectList[i];
+            obj->Move(timer.m_fSecondPerFrame);
+            quadtree.DynamicAddObject(obj);
+        }
+        
+        quadtree.Frame();
+        quadtree.Render();
+
+        system("cls");
+        std::cout << std::endl;
+        std::cout << "Object inform!\n";
+        quadtree.LevelOrder(quadtree.GetRootNode());
+        timer.Render();
+        Sleep(100); // tick 1000 => 1초
+    }
+    timer.Release();
+
+    for (int i = 0; i < StaticObjectList.size(); i++)
+    {
+        delete StaticObjectList[i];
+    }
+    StaticObjectList.clear();
+    for (int i = 0; i < DynamicObjectList.size(); i++)
+    {
+        delete DynamicObjectList[i];
+    }
+    DynamicObjectList.clear();
+
+    quadtree.Release();
+    
+}
---
+++ KGCA41/Quadtree3/TUtils.cpp
@@ -0,0 +1,1 @@
+#include "TUtils.h"
