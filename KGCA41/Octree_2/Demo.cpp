21b9d83ba7bc37f6b8bb33a09b28d21a7b332b19
prokvip <prokvip@msn.com>
2023-08-01 14:09:35 +0900

    map

--- KGCA41/Octree_2/Demo.cpp
+++ KGCA41/Octree_2/Demo.cpp
@@ -1,8 +1,14 @@
 ﻿#include "TQuadtree.h"
 #include "TOctree.h"
 #include "TTimer.h"
-std::vector<TObject3D*> StaticObjectList;
-std::vector<TObject3D*> DynamicObjectList;
+#include <map> // key+value   bst
+#include <unordered_map> 
+#include <queue>
+using MyList = std::unordered_map<int, TObject3D*>;
+MyList StaticObjectList;
+MyList DynamicObjectList;
+//std::vector<TObject3D*> StaticObjectList;
+//std::vector<TObject3D*> DynamicObjectList;
 
 void SetStaticObject(TOctree& tree)
 {    
@@ -11,7 +17,7 @@ void SetStaticObject(TOctree& tree)
     int iMaxWidth = tree.m_pRootNode->m_tBox.m_fWidth;
     int iMaxHeight = tree.m_pRootNode->m_tBox.m_fHeight;
     int iMaxDepth = tree.m_pRootNode->m_tBox.m_fDepth;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 1000; i++)
     {
         TObject3D* obj = new TStaticObject3D();
         obj->m_csName = L"StaticObj";
@@ -25,7 +31,7 @@ void SetStaticObject(TOctree& tree)
                     (float)((rand() % 10) + 1.0f));
         
         tree.StaticAddObject(obj);
-        StaticObjectList.push_back(obj);
+        StaticObjectList.insert(std::make_pair(i,obj));
     }
 }
 void SetDynamicObject(TOctree& tree)
@@ -35,7 +41,7 @@ void SetDynamicObject(TOctree& tree)
     int iMaxHeight = tree.m_pRootNode->m_tBox.m_fHeight;
     int iMaxDepth = tree.m_pRootNode->m_tBox.m_fDepth;
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 100000; i++)
     {
         TObject3D* obj = new TDynamicObject3D();
         
@@ -55,32 +61,143 @@ void SetDynamicObject(TOctree& tree)
                         (float)randstep(0, iMaxDepth) };
         obj->SetTarget(d);
         tree.DynamicAddObject(obj);
-        DynamicObjectList.push_back(obj);
+        DynamicObjectList.insert(std::make_pair(i, obj));        
     }
 }
-struct TData
+//struct TData
+//{
+//    TData* pNext;
+//};
+
+auto  Fun1()
 {
-    TData* pNext;
+    return std::pair<int, std::string>(1,"a");
+}
+auto  Fun2()
+{
+    return std::tuple<int, std::string,float>(1, "aaa", 3.14f);
+}
+auto  Fun3()
+{
+    return std::vector<int>(10);
+}
+struct cmp2
+{
+    bool operator()(int a, int b)
+    {
+        return a > b;
+    }
 };
 int main()
 { 
-    TData* pa = new TData;
-    pa->pNext = nullptr;
-    TData* pb = new TData;
-    pb->pNext = nullptr;
-    TData* pc = new TData;
-    pc->pNext = nullptr;
-    pa->pNext = pb;
-    pb->pNext = pc;
-    for (TData* pData = pa;
-        pData != nullptr;
-        pData = pData->pNext)
+    std::priority_queue<int> qList1;
+    for (int n : {3, 6, 23, 6, 7, 23, 7, 32, 5, 3, 6, 23, 643, 6345, 3})
     {
-        std::cout << pData << std::endl;
+        qList1.push(n);
     }
-    delete pa;
-    delete pb;
-    delete pc;
+
+    while (!qList1.empty())
+    {        
+        qList1.push(rand() % 1000);
+        std::cout << qList1.top() << " ";
+        qList1.pop();
+        std::cout << qList1.top() << " ";
+        qList1.pop();       
+    }
+    auto cmp = [](int a, int b)->bool { return a < b; };
+    std::priority_queue<int, std::vector<int>, std::greater<int>/*cmp2*/ > qList2;
+    for (int n : {3, 6, 23, 6, 7, 23, 7, 32, 5, 3, 6, 23, 643, 6345, 3})
+    {
+        qList2.push(n);
+    }
+
+    while (!qList2.empty())
+    {
+        qList2.push(rand() % 1000);
+        std::cout << qList2.top() << " ";
+        qList2.pop();
+        std::cout << qList2.top() << " ";
+        qList2.pop();
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    std::pair<int, std::string> r1 = Fun1();
+    std::tuple<int, std::string, float> r2 = Fun2();
+    std::vector<int> ret = Fun3();
+    
+    int iID;
+    std::string name;
+    std::tie(iID, name) = Fun1();   
+
+
+    using MyMap = std::map<int, std::string>;
+    std::pair<int, std::string> pairdata;
+    MyMap mapList1;
+    pairdata.first = 67;
+    pairdata.second = "혹길";
+    mapList1.insert(pairdata);
+
+    pairdata.first = 9;
+    pairdata.second = "강시";
+    mapList1.insert(pairdata);
+
+    //pairdata.first = 23;
+    //pairdata.second = "강3시";
+    mapList1.insert(std::make_pair(23, "강3시"));
+    mapList1.insert(std::make_pair(23, "강5시"));
+    /*mapList1[67] = "혹길";
+    mapList1[9] = "강시";
+    mapList1[23] = "강3시";
+    mapList1[67] = "2혹길";*/
+    MyMap::iterator d1 = mapList1.find(9);
+    
+    auto d3 = mapList1.find(19);
+    if (d3 == mapList1.end())
+    {
+        
+    }
+  
+    for (std::pair<int, std::string> d2 : mapList1)
+    {
+        int key = d2.first;
+        std::string value = d2.second;
+    }
+    for (auto& d7 : mapList1)
+    {
+        int key = d7.first;
+        std::string value = d7.second;
+    }
+    for (MyMap::iterator d3 = mapList1.begin();
+         d3 != mapList1.end();
+         d3++)
+    {
+     
+    }
+    for (int iKey = 0; iKey < mapList1.size(); iKey++)
+    {
+        std::cout << mapList1[iKey].c_str();
+    }
+    
+
+    //TData* pa = new TData;
+    //pa->pNext = nullptr;
+    //TData* pb = new TData;
+    //pb->pNext = nullptr;
+    //TData* pc = new TData;
+    //pc->pNext = nullptr;
+    //pa->pNext = pb;
+    //pb->pNext = pc;
+    //for (TData* pData = pa;
+    //    pData != nullptr;
+    //    pData = pData->pNext)
+    //{
+    //    std::cout << pData << std::endl;
+    //}
+    //delete pa;
+    //delete pb;
+    //delete pc;
 
 #pragma region ->사이각 반환(180내의 작은값)
     /// <summary>
@@ -152,7 +269,8 @@ int main()
     TTimer  timer;
     timer.Init();
     // 프레임 고정 100프레임    float fStep = 1.0f / 50.0f;
-    while (timer.m_fGameTimer < 1000.0f)
+    //while (timer.m_fGameTimer < 1000.0f)
+    while(1)
     {
         timer.Frame();       
         tree.PreFrame();
@@ -167,7 +285,7 @@ int main()
         tree.Frame();
         tree.Render();
 
-        system("cls");
+        //system("cls");
         std::cout << std::endl;
         std::cout << "Object inform!\n";
         for (int iObj=0; iObj < DynamicObjectList.size(); iObj++ )
@@ -177,7 +295,7 @@ int main()
         }
         //tree.LevelOrder(tree.m_pRootNode);
         timer.Render();
-        Sleep(100); // tick 1000 => 1초
+        //Sleep(100); // tick 1000 => 1초
     }
     timer.Release();
 
